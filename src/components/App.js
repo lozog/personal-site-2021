119ac9ded4ba50aad86088847bd969f8b05b7afb
Liam Ozog <liam.ozog@gmail.com>
2020-11-19 22:36:46 -0500

    add left nav and router

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 import Header from "components/Header";
 import "stylesheets/App.scss";
 
@@ -5,9 +7,26 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <p>I'm working on it, give me a sec</p>
+      <Router>
+        <div className="page-wrapper">
+          <nav className="nav-sidebar">
+            <Link to="/music">Music</Link>
+          </nav>
+          <div className="page-content">
+            <Switch>
+              <Route path="/music">
+                <Music />
+              </Route>
+            </Switch>
+          </div>
+        </div>
+      </Router>
     </div>
   );
+}
+
+function Music() {
+  return <h2>Music page under construction</h2>;
 }
 
 export default App;
