0d8297742aa8dd2195d2ec6159ac2ebc8f0713fb
Liam Ozog <liam.ozog@gmail.com>
2021-01-02 00:45:57 -0500

    replace internal href with Link

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 import "stylesheets/Home.scss";
 
 function Home() {
   return (
     <div className="Home">
-      Hi, I’m Liam. I like to <a href="https://github.com/lozog" rel="noopener noreferrer">build stuff</a> and <a href="/music" rel="noopener noreferrer">write songs</a>. I also like <a href="https://www.instagram.com/l__ozog/" rel="noopener noreferrer">taking photos on film</a> and I listen to <a href="https://www.last.fm/user/Tom_Swift" rel="noopener noreferrer">a lot</a> of music.
+      Hi, I’m Liam. I like to <a href="https://github.com/lozog" rel="noopener noreferrer">build stuff</a> and <Link to="/music">write songs</Link>. I also like <a href="https://www.instagram.com/l__ozog/" rel="noopener noreferrer">taking photos on film</a> and I listen to <a href="https://www.last.fm/user/Tom_Swift" rel="noopener noreferrer">a lot</a> of music.
     </div>
   );
 }
