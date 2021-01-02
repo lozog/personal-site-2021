7d93886afc798c053afc597e841500bcbf16b1ff
Liam Ozog <liam.ozog@gmail.com>
2021-01-01 22:36:11 -0500

    add machu-pichu gallery

--- src/data/galleries.js
+++ src/data/galleries.js
@@ -10,5 +10,11 @@ export const GALLERIES = {
     coverFilename: "000031370021_edited.gif",
     description: "Mexico City 2019",
     images: [{ "thumbnail": "000031370036_edited.gif", "filename": "000031370036_edited.jpg" }, { "thumbnail": "000031370033_edited.gif", "filename": "000031370033_edited.jpg" }, { "thumbnail": "000031370015_edited.gif", "filename": "000031370015_edited.jpg" }, { "thumbnail": "000031370012_edited.gif", "filename": "000031370012_edited.jpg" }, { "thumbnail": "000031370025_edited.gif", "filename": "000031370025_edited.jpg" }, { "thumbnail": "000031370034_edited.gif", "filename": "000031370034_edited.jpg" }, { "thumbnail": "000031370028_edited.gif", "filename": "000031370028_edited.jpg" }, { "thumbnail": "000031370031_edited.gif", "filename": "000031370031_edited.jpg" }, { "thumbnail": "000031370019_edited.gif", "filename": "000031370019_edited.jpg" }, { "thumbnail": "000031370008_edited.gif", "filename": "000031370008_edited.jpg" }, { "thumbnail": "000031370026_edited.gif", "filename": "000031370026_edited.jpg" }, { "thumbnail": "000031370037_edited.gif", "filename": "000031370037_edited.jpg" }, { "thumbnail": "000031370021_edited.gif", "filename": "000031370021_edited.jpg" }, { "thumbnail": "000031370016_edited.gif", "filename": "000031370016_edited.jpg" }, { "thumbnail": "000031370029_edited.gif", "filename": "000031370029_edited.jpg" }]
+  },
+  "machu-picchu": {
+    title: "Machu Picchu",
+    coverFilename: "000068140011-11.gif",
+    description: "Machu Picchu",
+    images: [{ "filename": "000068140013-13.jpg", "thumbnail": "000068140013-13.gif" }, { "filename": "000068140020-20.jpg", "thumbnail": "000068140020-20.gif" }, { "filename": "000068140011-11.jpg", "thumbnail": "000068140011-11.gif" }, { "filename": "000068140022-22.jpg", "thumbnail": "000068140022-22.gif" }, { "filename": "000068140024-24.jpg", "thumbnail": "000068140024-24.gif" }, { "filename": "000068140026-26.jpg", "thumbnail": "000068140026-26.gif" }, { "filename": "000068140019-19.jpg", "thumbnail": "000068140019-19.gif" }, { "filename": "000068140012-12.jpg", "thumbnail": "000068140012-12.gif" }, { "filename": "000068140014-14.jpg", "thumbnail": "000068140014-14.gif" }, { "filename": "000068140008-8.jpg", "thumbnail": "000068140008-8.gif" }]
   }
 };
