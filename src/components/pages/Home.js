726110676fb6cfcc0d8a8d01ae9a13a1e9dba85c
Liam Ozog <liam.ozog@gmail.com>
2021-01-01 20:49:17 -0500

    update homepage text

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,7 +3,7 @@ import "stylesheets/Home.scss";
 function Home() {
   return (
     <div className="Home">
-      Liam Ozog is a programmer, musician, and photographer from Toronto.
+      Hi, I’m Liam. I like to <a href="https://github.com/lozog">build stuff</a> and <a href="/music">write songs</a>. I also like <a href="https://www.instagram.com/l__ozog/">taking photos on film</a> and I listen to <a href="https://www.last.fm/user/Tom_Swift">a lot</a> of music.
     </div>
   );
 }
