2fa8b7372c0d34f20062bbd2b1ac79f33676c44c
Liam Ozog <liam.ozog@gmail.com>
2021-01-31 21:38:55 -0500

    move Home page to new component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import 'react-image-lightbox/style.css';
 
 import About from "components/About/index.js";
 import Footer from "components/Footer/index.js";
 import Header from "components/Header/index.js";
+import Home from "components/Home/index.js";
 import Gallery from "components/Gallery/index.js";
 // import HomeGallery from "components/pages/HomeGallery/index.js";
 import Music from "components/Music/index.js";
@@ -14,50 +15,14 @@ import "./styles.scss";
  * TODO: refactor using StyledComponents
  */
 
-function HomeTest() {
-  return (
-    <div className="HomeTest">
-      <div className="HomeTest__outer-container">
-        <div className="HomeTest__inner-container">
-          <Link to={"/music"}>
-            <div className="HomeTest__image-1">
-              <img src="https://f4.bcbits.com/img/a0642444381_16.jpg" alt="music" />
-              <div className="HomeTest__image-overlay">
-                <div className="HomeTest__image-overlay-contents">Music</div>
-              </div>
-            </div>
-          </Link>
-          <Link to={"/about"}>
-            <div className="HomeTest__image-2">
-              <img src={"/img/000017960026-26.jpg"} alt="me" />
-              <div className="HomeTest__image-overlay">
-                <div className="HomeTest__image-overlay-contents">About</div>
-              </div>
-            </div>
-          </Link>
-          <Link to={"/photography"}>
-            <div className="HomeTest__image-3">
-              <img src="/img/buenos-aires-2020/000008-3.jpg" alt="photography" />
-              <div className="HomeTest__image-overlay">
-                <div className="HomeTest__image-overlay-contents">Photography</div>
-              </div>
-            </div>
-          </Link>
-          <div className="HomeTest__clear"></div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Router>
         <Header />
         <Switch>
           <Route path="/" exact={true}>
-            <HomeTest />
+            <Home />
           </Route>
           <Route path="/music">
             <Music />
@@ -71,12 +36,10 @@ function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route component={HomeTest} />
+          <Route component={Home} />
         </Switch>
         <Footer />
       </Router>
     </div>
   );
 }
-
-export default App;
