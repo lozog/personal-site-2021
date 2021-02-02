b71bf13f2e0d8f3406898774323929ab8a22c3dd
Liam Ozog <liam.ozog@gmail.com>
2021-02-01 21:05:50 -0500

    make Home responsive

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,32 +5,32 @@ import "./styles.scss";
 export default function Home() {
   return (
     <div className="Home">
-      <div className="Home__outer-container">
-        <div className="Home__inner-container">
+      <div className="Home__container">
+        <div className="Home__centerpiece">
           <Link to={"/music"}>
-            <div className="Home__image-1">
+            <div className="Home__image Home__image--1">
               <img src="https://f4.bcbits.com/img/a0642444381_16.jpg" alt="music" />
               <div className="Home__image-overlay">
-                <div className="Home__image-overlay-contents">Music</div>
+                <div className="Home__image-overlay-text">Music</div>
               </div>
             </div>
           </Link>
           <Link to={"/about"}>
-            <div className="Home__image-2">
+            <div className="Home__image Home__image--2">
               <picture>
                 <source type="image/avif" srcset={"/img/000017960026-26.avif"} />
                 <img src={"/img/000017960026-26.jpg"} alt="me" />
               </picture>
               <div className="Home__image-overlay">
-                <div className="Home__image-overlay-contents">About</div>
+                <div className="Home__image-overlay-text">About</div>
               </div>
             </div>
           </Link>
           <Link to={"/photography"}>
-            <div className="Home__image-3">
+            <div className="Home__image Home__image--3">
               <img src="/img/buenos-aires-2020/000039-68.jpg" alt="photography" />
               <div className="Home__image-overlay">
-                <div className="Home__image-overlay-contents">Photography</div>
+                <div className="Home__image-overlay-text">Photography</div>
               </div>
             </div>
           </Link>
