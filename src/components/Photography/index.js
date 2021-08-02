1cd04220ee897fbdcc92103d7cdf00256d4cdc7b
Liam Ozog <liam.ozog@gmail.com>
2021-08-02 15:58:28 -0400

    replace Photography page with single Gallery page

--- src/components/Photography/index.js
+++ src/components/Photography/index.js
@@ -1,32 +1,60 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import Lightbox from 'react-image-lightbox';
 
 import Overlay from "components/Overlay";
-import { GALLERIES } from "data/galleries";
 import "./styles.scss";
 
-/**
- * TODO: compress images into avifs
- * https://github.com/GoogleChromeLabs/squoosh/tree/dev/cli
- */
+const gallery = {
+  title: "Buenos Aires",
+  coverFilename: "000008-3.jpg",
+  description: "Buenos Aires 2020",
+  images: [{ "filename": "000038-67.jpg" }, { "filename": "000012-7.jpg" }, { "filename": "000010-5.jpg" }, { "filename": "000016-11.jpg" }, { "filename": "000040-69.jpg" }, { "filename": "000036-65.jpg" }, { "filename": "000034-63.jpg" }, { "filename": "000011-6.jpg" }, { "filename": "000008-3.jpg" }, { "filename": "000039-68.jpg" }, { "filename": "000019-14.jpg" }, { "filename": "000035-64.jpg" }, { "filename": "000009-38.jpg" }, { "filename": "000013-8.jpg" }, { "filename": "000007-2.jpg" }, { "filename": "000042-71.jpg" }, { "filename": "000033-62.jpg" }]
+};
 
-function Photography() {
+export default function Photography() {
+  const [imageIndex, setImageIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const images = gallery.images.map(image => (
+    image.filename
+  ))
+
   return (
     <div className="Photography">
-      <div className="Photography__header">
-      </div>
       <div className="Photography__galleries-grid">
         {
-          Object.entries(GALLERIES).map(([key, galleryInfo], i) => (
-            <Link to={`/photography/${key}`} key={key}>
-              <Overlay text={galleryInfo.title}>
-                <img src={`/img/${key}/${galleryInfo.coverFilename}`} alt={galleryInfo.title} className="Photography__gallery-image" />
+          gallery.images.map((image, i) => (
+            <div
+              className="Photography__image-container"
+              key={image.filename}
+              onClick={() => {
+                setImageIndex(i)
+                setIsOpen(true)
+              }}
+            >
+              <Overlay>
+                <img src={`/img/photography/${image.filename}`} alt={image.filename} className="Photography__image" />
               </Overlay>
-            </Link>
+            </div>
           ))
         }
       </div>
+      {isOpen && (
+        <Lightbox
+          mainSrc={`/img/photography/${images[imageIndex]}`}
+          nextSrc={`/img/photography/${images[(imageIndex + 1) % images.length]}`}
+          prevSrc={`/img/photography/${images[(imageIndex + images.length - 1) % images.length]}`}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setImageIndex((imageIndex + images.length - 1) % images.length)
+          }
+          onMoveNextRequest={() =>
+            setImageIndex((imageIndex + 1) % images.length)
+          }
+          enableZoom={false}
+          animationDisabled
+        />
+      )}
     </div>
   );
 }
-
-export default Photography;
